320954791949c1902b0cc8be16eb71f8493d8600
Tatiana Ziakova <tatianaziakova@gmail.com>
2023-11-29 18:19:07 +0100

    installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
+      <button className="btn btn-primary">Hello</button>
       <footer>
         Coded by Tati 👩‍💻 as an <a href="https://github.com/TatiZofka/my-react-app" target="_blank" rel="noreferrer">
           open source project
         </a>
         .
       </footer>
+      </div>
     </div>
   );
 }
