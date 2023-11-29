c43e16727c178da8ea79cf2e1e9a320f38fa8195
Tatiana Ziakova <tatianaziakova@gmail.com>
2023-11-29 17:41:13 +0100

    added GitHub repository link for open source

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        Coded by Tati 👩‍💻 as an <a href="#" target="_blank">
+        Coded by Tati 👩‍💻 as an <a href="https://github.com/TatiZofka/my-react-app" target="_blank">
           open source project
         </a>
         .
