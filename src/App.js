7d871ba2e92a76ab20f108ffcdd0afe3dc3c4bbd
Tatiana Ziakova <tatianaziakova@gmail.com>
2023-11-29 18:41:40 +0100

    created main Weather component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
-      <button className="btn btn-primary">Hello</button>
+      <Weather />
       <footer>
         Coded by Tati 👩‍💻 as an <a href="https://github.com/TatiZofka/my-react-app" target="_blank" rel="noreferrer">
           open source project
